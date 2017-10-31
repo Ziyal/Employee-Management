dfedfda3fbd23731a33d09c05d3ef0d1ea867ed0
Sereina Blanchard <wintermist3@gmail.com>
2017-10-31 11:56:52 -0700

    styling to employees, title and new employee button added

--- employees.php
+++ employees.php
@@ -4,82 +4,50 @@
     //include header template
     require('layout/header.php');
 ?>
+<div class='container'>
+    <div class="employees-top-container">
+        <h1 class="employee-title">All Employees</h1>
+        <a href="employee_add.php" class="new-employee-btn"><button class="btn btn-success btn-lg">New Employee</button></a>
+    </div>
 
-<h1>All Employees</h1>
+    <?php require('config.php');
+        $stmt = $db->prepare('SELECT * FROM employees ORDER BY hire_date');
+        $stmt->execute();
+        $result = $stmt;
 
-<?php require('config.php');
-    // $sql = "SELECT * FROM employees ORDER BY hire_date";
-    $stmt = $db->prepare('SELECT * FROM employees ORDER BY hire_date');
-    $stmt->execute();
-    $result = $stmt;
-
-    if($result){
-        echo "<div class='employee-container'>";
-            echo "<table class='table table-hover'>";
-                echo "<thead>";
-                    echo "<tr>";
-                        echo "<th>Name</th>";
-                        echo "<th>Gender</th>";
-                        echo "<th>Birthday</th>";
-                        echo "<th>Date of Hire</th>";
-                        echo "<th>Email</th>";
-                        echo "<th>Edit</th>";
-                        echo "<th>Delete</th>";
-                    echo "</tr>";
-                echo "</thead>";
-            foreach($result as $row){
+        if($result){
+                echo "<table class='table table-striped table-condensed'>";
+                    echo "<thead>";
+                        echo "<tr>";
+                            echo "<th>Name</th>";
+                            echo "<th>Gender</th>";
+                            echo "<th>Birthday</th>";
+                            echo "<th>Date of Hire</th>";
+                            echo "<th>Email</th>";
+                            echo "<th>Edit</th>";
+                            echo "<th>Delete</th>";
+                        echo "</tr>";
+                    echo "</thead>";
+                    echo "<tbody>";
+                foreach($result as $row){
+                        echo "<tr>";
+                            echo "<td>" . $row['first_name'] . " " . $row['last_name'] . "</td>";
+                            echo "<td>" . $row['gender'] . "</td>";
+                            echo "<td>" . $row['birthday'] . "</td>";
+                            echo "<td>" . $row['hire_date'] . "</td>";
+                            echo "<td>" . $row['email'] . "</td>";
+                            echo "<td><button value=". $row['employee_id'] . " class='btn btn-primary btn-sm'>Edit</button></td>";
+                            echo "<td><form action='delete_employee/" . $row['employee_id'] . "' method='post'><input type='submit' value='Delete' class='btn btn-danger btn-sm'></form></td>";
+                        echo "</tr>";
+                }
                 echo "</tbody>";
-                    echo "<tr>";
-                        echo "<td>" . $row['first_name'] . " " . $row['last_name'] . "</td>";
-                        echo "<td>" . $row['gender'] . "</td>";
-                        echo "<td>" . $row['birthday'] . "</td>";
-                        echo "<td>" . $row['hire_date'] . "</td>";
-                        echo "<td>" . $row['email'] . "</td>";
-                        echo "<td><button value=". $row['employee_id'] . ">Edit</button></td>";
-                        echo "<td><form action='delete_employee/" . $row['employee_id'] . "' method='post'><input type='submit' value='Delete'></form></td>";
-                    echo "</tr>";
-                echo "</tbody>";
-            }
-            echo "</table>";
-        echo "</div>";
-        // Free result set
-        // mysqli_free_result($result);
-    } else{
-        echo "ERROR: Could not execute $sql. " . mysqli_error($db);
-    }
+                echo "</table>";
+        } else {
+            echo "ERROR: Could not execute $sql. " . mysqli_error($db);
+        }
+    ?>
 
-
-    // if($result = mysqli_query($db, $sql)){
-    //     if(mysqli_num_rows($result) > 0){
-    //         echo "<table>";
-    //             echo "<tr>";
-    //                 echo "<th>Name</th>";
-    //                 echo "<th>Gender</th>";
-    //                 echo "<th>Birthday</th>";
-    //                 echo "<th>Date of Hire</th>";
-    //                 echo "<th>Email</th>";
-    //             echo "</tr>";
-    //         while($row = mysqli_fetch_array($result)){
-    //             echo "<tr>";
-    //                 echo "<td>" . $row['first_name'] . "</td>";
-    //                 echo "<td>" . $row['gender'] . "</td>";
-    //                 echo "<td>" . $row['birthday'] . "</td>";
-    //                 echo "<td>" . $row['date_hired'] . "</td>";
-    //                 echo "<td>" . $row['email'] . "</td>";
-    //             echo "</tr>";
-    //         }
-    //         echo "</table>";
-    //         // Free result set
-    //         mysqli_free_result($result);
-    //     } else{
-    //         echo "No records matching your query were found.";
-    //     }
-    // } else{
-    //     echo "ERROR: Could not execute $sql. " . mysqli_error($db);
-    // }
-
-?>
-
+</div>
 
 <?php
     //include footer template
